4a925cc47a8ec22abedd89f2d817d242cd4153f5
“zhangjiwen” <1129802827@qq.com>
2021-08-23 16:23:16 +0800

    创建工程

---
+++ Actions/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Actions
+//
+//  Created by jiwen zhang on 2021/8/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Actions/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Actions
+//
+//  Created by jiwen zhang on 2021/8/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
