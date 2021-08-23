a31f689d5ee8894952e9350ac0499be8a4668d39
“zhangjiwen” <1129802827@qq.com>
2021-08-23 16:36:19 +0800

    工程配置修改

--- Actions/AppDelegate.m
+++ Actions/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -15,26 +16,41 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    ViewController *vc = [[ViewController alloc] init];
+    UINavigationController *navi = [[UINavigationController alloc] initWithRootViewController:vc];
+    UIWindow *w = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    w.rootViewController = navi;
+    self.window = w;
+    [w makeKeyAndVisible];
+
+
     return YES;
 }
+- (void)applicationWillResignActive:(UIApplication *)application {
 
-
-#pragma mark - UISceneSession lifecycle
-
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
 }
 
 
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+
 }
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+
+}
+
+
 
 
 @end
--- Actions/ViewController.m
+++ Actions/ViewController.m
@@ -15,7 +15,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn setTitle:@"测试" forState: UIControlStateNormal];
+    btn.backgroundColor = UIColor.redColor;
+    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    btn.frame = CGRectMake(100, 100, 100, 100);
+    [self.view addSubview:btn];
+    
+    
+    [super viewDidLoad];
+    UIButton *btn1 = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn1 setTitle:@"修改" forState: UIControlStateNormal];
+    btn1.backgroundColor = UIColor.redColor;
+    [btn1 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    btn1.frame = CGRectMake(250, 250, 100, 100);
+    [self.view addSubview:btn1];
+    
+    [super viewDidLoad];
+    UIButton *btn2 = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn2 setTitle:@"再修改13" forState: UIControlStateNormal];
+    btn2.backgroundColor = UIColor.redColor;
+    [btn2 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    btn2.frame = CGRectMake(150, 450, 100, 100);
+    [self.view addSubview:btn2];
 }
 
 
